137a74d8d5ac1b8bdeec0b00d694e0e809ffbe73
Kouakou Neiba Aristide <guyaristide@gmail.com>
2019-06-18 19:57:17 +0000

    tuto model

--- dist/ITutorial.d.ts
+++ dist/ITutorial.d.ts
@@ -1,11 +1,10 @@
 import { BaseEntityInterface } from "./BaseEntityInterface";
 import { ICloudImage } from "./ICloudImage";
-
 export interface ITutorial extends BaseEntityInterface {
-  lablel: string;
-  description: string;
-  full_description?: string;
-  professionsIds?: string[];
-  tags?: string[];
-  cover?: ICloudImage;
+    lablel: string;
+    description: string;
+    full_description?: string;
+    professionsIds?: string[];
+    tags?: string[];
+    cover?: ICloudImage;
 }
